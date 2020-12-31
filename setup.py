8bef93140fa55c0f464919cbafde225470cf49e4
Duc Anh Nguyen <anh.nguyenducit@gmail.com>
2020-12-31 14:09:15 +0100

    update _add hyperopt

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='BO4ML',
     version='0.1.0',
-    packages=['BanditOpt', 'Component', 'Component.BayesOpt'],
+    packages=['BanditOpt', 'Component', 'Component.BayesOpt','Component.mHyperopt'],
     url='http://hyperparamter.ml',
     license='MIT',
-    author='Duc Anh Nguyen, Hao Wang, Thomas Bäck',
+    author='Duc Anh Nguyen, Thomas Bäck',
     author_email='d.a.nguyen[at]liacs.leidenuniv[dot]nl',
-    description='Machine learning - Hyperparameter optimisation tool (MIP-EGO4ML upgrade)',
+    description='Machine learning - Hyperparameter optimisation tool (BO4ML upgrade)',
     #install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'joblib', 'dill','BayesOpt']
 )
