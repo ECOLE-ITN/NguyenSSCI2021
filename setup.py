01190c4a8d143ace378bf4fe3ed3886fb71aece8
Duc Anh Nguyen <38556234+anh05@users.noreply.github.com>
2021-10-09 14:11:06 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1.2',
     packages=['BanditOpt', 'Component','Component.mHyperopt'],
     url='http://hyperparameter.ml',
-    license='MIT',
+    license='GPL-3.0 License',
     author='Duc Anh Nguyen',
     author_email='d.a.nguyen@liacs.leidenuniv.nl',
-    description='Divide and conquer strategy for Full model selection (Combined Model selection and Hyperparameter optimisation)',
+    description='BO4AutoML: Bayesian Optimization library AutoML',
     install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'joblib', 'dill','hyperopt']
 )
