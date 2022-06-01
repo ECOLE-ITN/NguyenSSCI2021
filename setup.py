e520282d126161c79a87f460f81c0f9e83ee8a85
Duc Anh Nguyen <38556234+anh05@users.noreply.github.com>
2022-06-01 23:13:26 +0200

    small update

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name='BO4ML',
     version='0.1',
@@ -10,6 +16,11 @@ setup(
     license='GPL-3.0 License',
     author='Duc Anh Nguyen',
     author_email='d.a.nguyen@liacs.leidenuniv.nl',
-    description='BO4AutoML: Bayesian Optimization library AutoML',
-    install_requires=requirements
+    description='BO4ML: Bayesian Optimization library AutoML',
+    long_description=long_description,  # Optional
+    description_content_type='text/markdown',
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='AutoML optimization, Combination sampling, Bayesian Optimization',  # Optional
 )
